fe4e6c3965e1b6235bf9bc74aadac293c386ab67
mahdie <mahdie.ahmadii97@gmail.com>
2022-11-12 16:55:58 +0330

    style: add color palette

--- src/components/Contact/SearchContact/index.js
+++ src/components/Contact/SearchContact/index.js
@@ -1,10 +1,11 @@
+import { PURPLE } from '../../../helpers/colors/index'
 export const SearchContact = () => {
     return (
         <div className="input-group mx-2 w-75" dir="ltr">
-            <span className="input-group-text" id="basic-addon1" style={{ backgroundColor: "purple" }}>
+            <span className="input-group-text" id="basic-addon1" style={{ backgroundColor: PURPLE }}>
                 <i className="fas fa-search" />
             </span>
-            <input dir="rtl" type="text" style={{ backgrounColor: "gray", borderColor: "purple" }} className="form-control" placeholder="جستجوی مخاطب" aria-label="search" aria-describedby="basic-addon1" />
+            <input dir="rtl" type="text" className="form-control" placeholder="جستجوی مخاطب" aria-label="search" aria-describedby="basic-addon1" />
         </div>
     )
 }
