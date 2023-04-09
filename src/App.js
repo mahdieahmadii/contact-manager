f491c66f5dd4aec35a57c01d78fb1c627248eb8d
Mahdie Ahmadi <mahdieahmadii97@gmail.com>
2023-04-09 22:02:56 +0430

    initialize project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <h3>اپلیکیشن مدیریت مخاطبین</h3>
+      <button className="btn btn-primary">click</button>
+    </div>
+  );
+};
+
+export default App;
