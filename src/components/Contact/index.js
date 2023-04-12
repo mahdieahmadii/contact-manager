1c59b3e9fd584afe4c65ddd06767ce55fe2c0384
Mahdie Ahmadi <mahdieahmadii97@gmail.com>
2023-04-12 23:54:26 +0430

    add color palette

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,13 +1,20 @@
 import SearchContact from "./SearchContact";
-
+import { PURPLE, BACKGROUND } from "../../helpers/colors";
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-dark navbar-expand-sm shadow-lg">
+    <nav
+      className="navbar navbar-dark navbar-expand-sm shadow-lg"
+      style={{
+        backgroundColor: BACKGROUND,
+      }}
+    >
       <div className="container">
         <div className="row w-100">
           <div className="col">
-            <i className="fas fa-id-badge" />
-            وب اپلیکیشن مدیریت <span style={{ color: "purple" }}>مخاطبین</span>
+            <div className="navbar-brand">
+              <i className="fas fa-id-badge" style={{ color: PURPLE }} /> وب
+              اپلیکیشن مدیریت <span style={{ color: PURPLE }}>مخاطبین</span>
+            </div>
           </div>
           <div className="col">
             <SearchContact />
