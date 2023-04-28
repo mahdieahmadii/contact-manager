d285d5fb6272a68a614bfe6649650f3928518af7
Mahdie Ahmadi <mahdieahmadii97@gmail.com>
2023-04-28 16:24:58 +0430

    add json server

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import { useState } from "react";
 import "./App.css";
 import Contacts from "./components/Contact/Contacts";
+import AddContact from "./components/Contact/AddContact";
+import EditContact from "./components/Contact/EditContact";
+import ViewContact from "./components/Contact/ViewContact";
 import Navbar from "./components/Contact/index";
+import { Navigate, Route, Routes } from "react-router-dom";
 
 const App = () => {
   const [getContacts, setContacts] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
   return (
     <div className="App">
       <Navbar />
-      <Contacts contacts={getContacts} loading={loading} />
+
+      <Routes>
+        <Route path="/" element={<Navigate to="/contacts" />} />
+        <Route
+          path="/contacts"
+          element={<Contacts contacts={getContacts} loading={loading} />}
+        />
+        <Route path="/comtacts/add" element={<AddContact />} />
+        <Route path="/contacts/:contactId" element={<ViewContact />} />
+        <Route path="/contacts/edit/:contactId" element={<EditContact />} />
+      </Routes>
     </div>
   );
 };
